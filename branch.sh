c022627143c39b331199b9e76aa2ea0a0db895dc
dejan-crocoder <dejan@crocoder.dev>
2023-08-18 18:04:41 +0200

    first commit

---
+++ branch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git checkout -b feat/branch
+echo -e "\ndef" >> to_append
+echo -e "\ndef" >> to_move_to_kept_append
+echo "def" > to_move_to_kept_overwrite
+rm to_remove
+mv to_move_to_kept kept
+mv to_move_to_kept_append kept
+mv to_move_to_kept_overwrite kept
+mv to_rename renamed
+git add .
+git commit -m "branch commit"
