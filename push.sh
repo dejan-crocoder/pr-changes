dc1e0a8d21ca4a263c19749f9adac3b8d8970f86
dejan-crocoder <dejan@crocoder.dev>
2023-08-23 16:55:33 +0200

    first commit

---
+++ push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -z "$1" ]
+  then
+    echo "missing <origin>. usage: ./push.sh <origin>"
+    exit 1
+fi
+
+git remote add origin $1
+git checkout main && git push --force origin main
+git checkout feat/branch && git push --force origin feat/branch
+git checkout feat/branch-60-diffs && git push --force origin feat/branch-60-diffs
