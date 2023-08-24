1a46df8227c71b86e73ad5c79978fe9f12b61c39
dejan-crocoder <dejan@crocoder.dev>
2023-08-24 09:46:01 +0200

    first commit

---
+++ branch-60-diffs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git checkout main
+git checkout -b feat/branch-60-diffs
+mkdir diffs
+for i in 0{1..60}
+do
+  echo "diff-${i}" > diffs/diff-${i}
+done
+git add .
+git commit -m "branch-60-diffs commit"
---
+++ branch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git checkout main
+git checkout -b feat/branch
+echo "def" >> to_append
+echo "def" >> to_move_to_kept_append
+echo "kept/to_move_to_kept_overwrite" > to_move_to_kept_overwrite
+rm to_remove
+mv to_move_to_kept kept
+mv to_move_to_kept_append kept
+mv to_move_to_kept_overwrite kept
+mv to_rename renamed
+cat test_additions_deletions_feat_branch > test_additions_deletions
+cat test_hunks_lookalike_feat_branch > test_hunks_lookalike
+git add .
+git commit -m "branch commit"
